08214c5136114ae202cc379afaba1cbf9712553f
unknown <kalilmurian@gmail.com>
2025-11-10 19:41:46 -0300

    Versão Finalizada 1.1 (alerts)

--- alerts.js
+++ alerts.js
@@ -23,40 +23,7 @@ configurarAlerta("palestras-form", "alerta-palestras");
 configurarAlerta("inscricao-form", "alerta-inscricao");
 configurarAlerta("voluntariado-form", "alerta-voluntariado");
 
-/*const contactForm = document.getElementById("contact-form");
-const aContactForm = document.getElementById("alerta-contato");
 
-if (contactForm && aContactForm) {
-  contactForm.addEventListener('submit', function(e){
-    e.preventDefault();
-
-    aContactForm.classList.remove('d-none');
-    aContactForm.classList.add('show');
-    setTimeout(() => {
-      aContactForm.classList.remove("show");
-      aContactForm.classList.add("d-none");
-    }, 3000);
-    contactForm.reset();
-  });
-}
-
-
-const palestrasForm = document.getElementById("palestras-form");
-const aPalestrasForm = document.getElementById("alerta-palestras");
-
-if (palestrasForm && aPalestrasForm){
-    palestrasForm.addEventListener('submit', function(e){
-        e.preventDefault();
-
-        aPalestrasForm.classList.remove('d-none');
-        aPalestrasForm.classList.add('show');
-        setTimeout(() => {
-            aPalestrasForm.classList.remove('show');
-            aPalestrasForm.classList.add('d-none');
-        }, 3000);
-        palestrasForm.reset();
-    });
-}*/
 
 const doacaoBtn = document.getElementById("pix-btn");
 const alertDoacao = document.getElementById("alerta-chavePix");
